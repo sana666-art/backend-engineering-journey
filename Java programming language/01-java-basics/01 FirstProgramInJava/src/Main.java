e1d462a1308e8b1e76984804d6c68057fe6b1dd6
Sana Khalid <102246653+sana666-art@users.noreply.github.com>
2026-03-01 22:09:54 +0500

    Setup intellij idea and run First Program

---
+++ Java programming language/01-java-basics/01 FirstProgramInJava/src/Main.java
@@ -0,0 +1,7 @@
+package FirstProject.src;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To My Backend Learning Series.");
+        System.out.println("Sana Khalid - Full Stack Developer");
+    }
+}
